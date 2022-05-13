843f0fa57ef1fa3377fdd42e31616ee428e5924b
Vincent Tjia <vtjia51@gmail.com>
2022-05-13 03:47:54 -0400

    add inquirer for intern

--- index.js
+++ index.js
@@ -136,7 +136,82 @@ const questions = {
 
     Intern: [
         {
-            
+            type: "input",
+            name: "name",
+            message: "What is the name of the intern",
+            validate: (value) => {
+                if (value) {
+                    return true
+                }
+                else {
+                    return "Plese enter intern's name."
+                }
+            },
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's ID?",
+            validate: (value) => {
+                if (value) {
+                    return true
+                }
+                else {
+                    return "Plese enter intern's name."
+                }
+            },
+        },
+        {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?",
+                validate: (value) => {
+                    if (value) {
+                        return true
+                    }
+                    else {
+                        return "Plese enter intern's name."
+                    }
+            },
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school does the intern go to?",
+            validate: (value) => {
+                if (value) {
+                    return true
+                }
+                else {
+                    return "Plese enter the name of the school."
+                }
+            },
+        },
+        {
+            type: "list",
+            name: "addEmployee",
+            message: "Do you want to add another employee?",
+            choices: ["yes", "no"]
         }
     ]
 };
+
+const selectEmployeeType = [
+    {
+        type: "list",
+        name: "employeeType",
+        message: "Select role of the employee",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+];
+
+function addNewEmployee() {
+    inquirer
+    .prompt(selectEmployeeType)
+    .then(data => {
+        // console.log(data.employeeType)
+        if (data.employeeType === "Manager") {
+
+        }
+    });
+}
